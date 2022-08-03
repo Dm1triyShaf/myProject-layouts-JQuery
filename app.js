13039bd2986cd3bff571319122f9212a12eac973
Dmitriy Shafalyuk <97801899+Dm1triyShaf@users.noreply.github.com>
2022-08-04 00:20:19 +0300

    firstCommit

---
+++ app.js
@@ -0,0 +1,16 @@
+$(function() {
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = 0;
+
+
+    $(window).on("scroll", function() {
+
+        scrollOffset = $(this).scrollTop();
+
+       console.log(scrollOffset);
+        
+    });
+
+});
