9580ce55c36223983802cf107db26b1eb38f7736
Dmitriy Shafalyuk <97801899+Dm1triyShaf@users.noreply.github.com>
2022-08-04 11:06:39 +0300

    firstCommit

--- app.js
+++ app.js
@@ -23,14 +23,14 @@ $(function() {
         if(scrollOffset >= introH) {
             header.addClass("fixed");
         } else {
-            header.removeClass("fixed")
+            header.removeClass("fixed");
         }
     }
 
     // Smooth scroll
 
     $("[data-scroll").on("click",function(event){
-        event.preventDefault()
+        event.preventDefault();
 
 
         var $this = $(this)
@@ -38,13 +38,23 @@ $(function() {
             blockOffset = $(blockId).offset().top; 
         
 
-        $("#nav a").removeClass("active")    
+        $("#nav a").removeClass("active");   
         $this.addClass("active");
 
         $("html,body").animate({
             scrollTop: blockOffset
-        }, 800)
+        }, 800);
     })
+
+
+    // menu nav toggle
+
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
 
 
 
